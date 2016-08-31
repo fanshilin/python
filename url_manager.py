00b4d91b9b0b158a8a3b124d75e955773dfa0ee0
fanshilin <283587493@qq.com>
2016-08-31 18:44:39 +0800

    python爬虫代码

---
+++ url_manager.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+
+
+class UrlManager(object):
+    def __init__(self):
+        self.new_urls = set()  # 这里建立的是集合 集合可以自动判断是否存在，
+        self.old_urls = set()  # 集合.add方法仅能增加与已有数据不重复的项目
+
+    def add_new_url(self, url):
+        if url is None:
+            return
+        if url not in self.new_urls and url not in self.old_urls:
+            self.new_urls.add(url)
+
+    def add_new_urls(self, urls):
+        if urls is None or len(urls) == 0:
+            return
+        for url in urls:
+            self.add_new_url(url)
+
+    def has_new_url(self):
+        return len(self.new_urls) != 0
+
+    def get_new_url(self):
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
